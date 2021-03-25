1ffa6479d6f5a13039924d36013307ec0163c15c
DavidRambolajaona <david.rambolajaona@esti.mg>
2021-03-25 08:30:35 +0300

    Configuring deployment for Heroku

--- davidapiapp/src/blueprints/geolocation/routes.py
+++ davidapiapp/src/blueprints/geolocation/routes.py
@@ -12,8 +12,8 @@ def getLocationApi() :
     driver = getDriver()
     timeout = 20
     driver.get("https://mycurrentlocation.net/")
-    wait = WebDriverWait(driver, timeout)
-    time.sleep(3)
+    # wait = WebDriverWait(driver, timeout)
+    # time.sleep(3)
     longitude = driver.find_elements_by_xpath('//*[@id="longitude"]') #Replace with any XPath    
     longitude = [x.text for x in longitude]    
     longitude = str(longitude[0])    
@@ -24,8 +24,21 @@ def getLocationApi() :
     return jsonify({"geocoding": {"longitude": longitude, "latitude": latitude}})
 
 def getDriver():
+    # Configuring options
     options = Options()
     options.add_argument("--use--fake-ui-for-media-stream")
-    dir_path = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/")
-    driver = webdriver.Chrome(executable_path = dir_path + "/chromedriver.exe",options=options)
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+    binary_location_chrome = os.environ.get("GOOGLE_CHROME_BIN")
+    if binary_location_chrome is not None :
+        # This is in production
+        options.add_argument("--headless")
+        options.add_argument('--disable-gpu')
+        options.binary_location = binary_location_chrome
+    executable_path_chrome = os.environ.get("CHROMEDRIVER_PATH")
+    if executable_path_chrome is None :
+        dir_path = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/")
+        executable_path_chrome = dir_path + "/chromedriver.exe"
+    
+    driver = webdriver.Chrome(executable_path = executable_path_chrome,options=options)
     return driver
